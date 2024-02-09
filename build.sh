670030e31aa503619f28d1b9fd68e6a69f41c50c
pieersx <piers_84@hotmail.com>
2024-02-08 20:46:34 -0500

    Compilation script with clang

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# export PKG_CONFIG_PATH=$HOME/opt/raylib/lib/pkgconfig/
+set -xe
 
 RAYLIB_DIR="./raylib"
 
-# Compilar y enlazar el programa
 clang -I"${RAYLIB_DIR}/include" -o sortalizer main.c -Wall -Wextra -L"${RAYLIB_DIR}/lib" -lraylib -lm && ./sortalizer
