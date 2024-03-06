9cab47b47de96794b2ad014664c513ebc9ae0deb
pieersx <piers_84@hotmail.com>
2024-03-06 03:51:46 -0500

    Update lgorithm

--- main.c
+++ main.c
@@ -3,71 +3,66 @@
 #include <time.h>
 #include <stdbool.h>
 
-#include "raylib.h"
+#include <raylib.h>
 
-#define train_count sizeof(vector)/ sizeof(vector[0])
+#include "sort.h"
 
-const int screenWidth = 700;
-const int screenHeight = 700;
+#define COLOR_BACKGROUND GetColor(0x282828FF)
+#define TRAIN_COUNT      sizeof(vector)/ sizeof(vector[0])
 
-bool shuffle (int n, int vector[]) 
-{
-    for (int i = 0; i < screenWidth; ++i) {
-        if (n == vector[i]) {
-            return true;
-        }
-    }
-    return false;
-}
+const int screenWidth = 100;
+const int screenHeight = 720;
 
 int main ()
 {
-    InitWindow(screenWidth, screenHeight, "sortalizer");
-    SetTargetFPS(60);
+	size_t screen = 80;
+	Image logo = LoadImage("./resources/icons/sort.png");
+	InitWindow(screen*16.25, screen*9, "Sortalizer");
+	SetTargetFPS(10);  
+	srand(time(0));
+	int vector[screenWidth];
+	int n = 0;
 
-    srand(time(0));
-    int vector[screenWidth], n;
+	for (size_t i = 0; i < TRAIN_COUNT; ++i) {       
+		while (shuffle(n, vector)) {
+			n = 1 + rand () % screenWidth;
+		}
+		vector[i] = n;
+	}
+	int cur = 0;
+	int index = 0;
+	int min_val = screenWidth + 1;
+	
+	while (!WindowShouldClose()) {
+		ClearBackground (COLOR_BACKGROUND);
+		SetWindowIcon(logo);
+		BeginDrawing();
+		{
+			if (cur < screenWidth) {
+				for (size_t i = cur; i < TRAIN_COUNT; ++i) {
+					if (vector[i] < min_val) {
+						index = i;
+						min_val = vector[i];
+					}
+				}
+				vector[index] = vector[cur];
+				vector[cur] = min_val;
+				cur++;
+				min_val = screenHeight + 1;
 
-    for (size_t i = 0; i < train_count; ++i) {
-        while (shuffle(n, vector)) {
-            n = 1 + rand () % 700;
-        }
-        vector[i] = n;
-    }
-
-    double curTime = GetTime();
-    int cur = 0;
-    int index = 0;
-    int min_val = screenHeight + 1;
-
-    while (!WindowShouldClose()) {
-        ClearBackground (BLACK);
-
-        BeginDrawing();
-        {
-            if (GetTime() > curTime) {
-                if (cur < screenWidth) {
-                    for (size_t i = cur + 1; i < train_count; ++i) {
-                        if (vector[i] < min_val) {
-                            index = i;
-                            min_val = vector[i];
-                        }
-                    }
-                    vector[index] = vector[cur];
-                    vector[cur] = min_val;
-                    cur++;
-                    min_val = screenHeight;
-
-                }
-                for (size_t i = 0; i < train_count; ++i) {
-                    DrawRectangle(i, 0, 1, vector[i], GREEN);
-                }
-            }
-        }
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-    return 0;
+			}
+		
+			for (size_t i = 0; i < TRAIN_COUNT; ++i) {
+				float hue = (float)i/TRAIN_COUNT*14.5;
+				float saturation = 0.75f;
+				float value = 1.0f;
+				Color color = ColorFromHSV(hue*360, saturation, value);	        
+				DrawRectangle(i*13, screenHeight - (vector[i]+3)*7, 12,(vector[i]+3)*7, color);
+			}
+		}
+		EndDrawing();
+	}
+	CloseWindow();
+	UnloadImage(logo);
+	return 0;
 }
